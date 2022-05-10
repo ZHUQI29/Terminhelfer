f037016ef1a2d0c1fa4faa42dbf039bb953ee4e8
ZHUQI29 <if21b206@technikum-wien.at>
2022-05-10 19:39:19 +0200

    。

--- WEB-Projekt-SS2021 _ZHU/htdocs/backend/model/user.php
+++ WEB-Projekt-SS2021 _ZHU/htdocs/backend/model/user.php
@@ -11,16 +11,16 @@
         //create a function to get all users
         //创建一个函数来获取所有用户
         public function getUsers() {
-            $sql = "SELECT * FROM users";
+            $sql = "SELECT * FROM user";
             $result = $this->db->runQuery($sql, "select");
-            //return all users
+            //return all user
             $users = $this->db->fetch_all($result);
             return $users;
         }
         //create a function to get a single user
         //创建一个函数来获取单个用户
         public function getUser($id) {
-            $sql = "SELECT * FROM users WHERE ID = $id";
+            $sql = "SELECT * FROM user WHERE Pid = $id";
             $result = $this->db->runQuery($sql, "select");
             return $result->fetch_assoc();
         }
@@ -34,28 +34,28 @@
         //create a function to add a user
         //创建一个添加用户的函数
         public function addUser($firstname, $lastname, $email,$username, $password, $birthday) {
-            $sql = "INSERT INTO users(firstname,lastname,email,username,password,birthday) VALUES('$firstname','$lastname','$email','$username','$password','$birthday')";
+            $sql = "INSERT INTO user(username,password) VALUES('$username','$password'),personaldata(firstname,lastname,email,birthday) VALUES('$firstname','$lastname','$email','$birthday'";
             $result = $this->db->runQuery($sql,"insert");
             return $result;
         }
         //create a function to update a user
         //创建一个函数来更新用户
         public function updateUser($id, $firstname, $lastname, $email,$username, $password, $birthday) {
-            $sql = "UPDATE users SET firstname = '$firstname', lastname = '$lastname', email = '$email', username = '$username', password = '$password', birthday = '$birthday' WHERE id = $id";
+            $sql = "UPDATE user SET username = '$username', password = '$password' WHERE Pid = $id";
             $result = $this->db->runQuery($sql,"update");
             return $result;
         }
         //create a function to delete a user
         //创建一个删除用户的函数
         public function deleteUser($id) {
-            $sql = "DELETE FROM users WHERE id = $id";
+            $sql = "DELETE FROM user,personaldata WHERE Pid = $id";
             $result = $this->db->runQuery($sql,"delete");
             return $result;
         }
         //create a function to check if username exists
         //创建一个函数来检查用户名是否存在
         public function checkUsername($username) {
-            $sql = "SELECT * FROM users WHERE username = '$username'";
+            $sql = "SELECT * FROM user WHERE username = '$username'";
             $result = $this->db->runQuery($sql, "select");
             if($result->num_rows > 0) {
                 return true;
@@ -66,7 +66,7 @@
         //create a function to check if email exists
         //创建一个函数来检查电子邮件是否存在
         public function checkEmail($email) {
-            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $sql = "SELECT * FROM personaldata WHERE email = '$email'";
             $result = $this->db->runQuery($sql, "select");
             if($result->num_rows > 0) {
                 return true;
